151c763ac531208fbea3c1363f2b0958f8ac3030
renan jegouzo <renan@aestesis.org>
2025-11-07 02:46:42 +0100

    lil clean Store

--- lib/src/store.dart
+++ lib/src/store.dart
@@ -5,35 +5,37 @@ import 'package:shared_preferences/shared_preferences.dart';
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
 class Store {
-  static late final SharedPreferences prefs;
-  static Future<void> initialize() async {
-    prefs = await SharedPreferences.getInstance();
-  }
-
+  static SharedPreferences? _prefs;
   static Future<Iterable<String>> keys({String? prefix}) async {
-    final keys = prefs.getKeys();
+    _prefs ??= await SharedPreferences.getInstance();
+    final keys = _prefs!.getKeys();
     if (prefix == null) return keys;
     return keys.where((k) => k.startsWith(prefix));
   }
 
   static Future<bool> contains(String key) async {
-    return prefs.containsKey(key);
+    _prefs ??= await SharedPreferences.getInstance();
+    return _prefs!.containsKey(key);
   }
 
   static Future<void> clear() async {
-    await prefs.clear();
+    _prefs ??= await SharedPreferences.getInstance();
+    await _prefs!.clear();
   }
 
   static Future<void> remove(String key) async {
-    await prefs.remove(key);
+    _prefs ??= await SharedPreferences.getInstance();
+    await _prefs!.remove(key);
   }
 
   static Future<void> write(String key, dynamic json) async {
-    await prefs.setString(key, jsonEncode(json));
+    _prefs ??= await SharedPreferences.getInstance();
+    await _prefs!.setString(key, jsonEncode(json));
   }
 
   static Future<dynamic> read(String key) async {
-    var content = prefs.getString(key);
+    _prefs ??= await SharedPreferences.getInstance();
+    var content = _prefs!.getString(key);
     if (content != null) return jsonDecode(content);
     throw ArgumentError('key $key not found');
   }
