9e7dd4df032cee4ca34d796e93eeed33812a7710
renan jegouzo <renan@aestesis.org>
2026-01-23 01:45:11 +0100

    plop

--- lib/bb.dart
+++ lib/bb.dart
@@ -4,4 +4,3 @@ export 'src/extension.dart';
 export 'src/signal.dart';
 export 'src/store.dart';
 export 'src/ui.dart';
-export 'src/utils.dart';
--- lib/src/bb.dart
+++ lib/src/bb.dart
@@ -310,29 +310,6 @@ class BB {
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
-class Range<T extends num> {
-  final T? min;
-  final T? max;
-  const Range({this.min, this.max});
-  const Range.at(T p, {T? margin})
-    : min = (p - (margin ?? 0)) as T,
-      max = (p + (margin ?? 0)) as T;
-  Range<T> expand(T margin) =>
-      Range(min: (min! - margin) as T, max: (max! + margin) as T);
-  @override
-  String toString() => 'Range<${T.runtimeType}>(min:$min, max:$max)';
-  @override
-  bool operator ==(Object other) =>
-      other is Range<T> && min == other.min && max == other.max;
-  @override
-  int get hashCode => min.hashCode & max.hashCode;
-  static Range<double> get zero => const Range<double>.at(0.0);
-  static Range<double> get infinity =>
-      const Range<double>(min: double.negativeInfinity, max: double.infinity);
-}
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////////////////////////
 final Uint8List kTransparentImage = Uint8List.fromList(<int>[
   0x89,
   0x50,
